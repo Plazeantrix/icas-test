6a1fdd8222f48e4e1a88a6bb4eca4804ad7cf0a9
Sean <plazeantrix@gmail.com>
2021-01-26 11:53:26 +0800

    test4

--- WindowsApplication2/WindowsApplication2/Form2.Designer.vb
+++ WindowsApplication2/WindowsApplication2/Form2.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(819, 429)
+        Me.ClientSize = New System.Drawing.Size(894, 303)
         Me.Name = "Form2"
         Me.Text = "Form2"
         Me.ResumeLayout(False)
